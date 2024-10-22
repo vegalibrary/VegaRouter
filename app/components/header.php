5f58584441de99ba59c1abcac997f83482b89183
Vega Library <161152379+vegalibrary@users.noreply.github.com>
2024-10-22 16:40:17 +0100

    $$

--- app/components/header.php
+++ app/components/header.php
@@ -1,33 +1,4 @@
 <nav>
-    <div class="logo">
-        <h3>ScMedia</h3>
-    </div>
-    <div class="nav-actions">
-        <a class="nav-link" href="/jobs">
-            <span>Jobs</span>
-        </a>
-        
-        <button class="nav-link">
-            <div class="button">
-                <span>Me</span>
-            </div>
-
-            <div class="actions">
-                <div>
-                    <a class="action" href="/account/profile">
-                        <span>Profile</span>
-                    </a>
-                    <a class="action" href="/account/profile">
-                        <span>Settings</span>
-                    </a>
-                </div>
-                <hr>
-                <div>
-                    <a class="action" href="/account/profile">
-                        <span>Log out</span>
-                    </a>
-                </div>
-            </div>
-        </button>
-    </div>
+    <h2><?= $title ?? "My website" ?></h2>
+    <a href="/">Home</a> | <a href="/user/1">User 1</a>
 </nav>
