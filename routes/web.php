f84f4094df9721f685243765bf5ba0fcc271470e
Vega Library <161152379+vegalibrary@users.noreply.github.com>
2024-10-22 16:48:00 +0100

    VEGA Library

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,35 @@ $router = new Router();
 
 $router->layout('default'); 
 
+// Public routes
 
-
+// run : http://localhost/
 $router->get('/', function() use ($router) {
     $router->render('public/home');
 });
 
-$router->get('/about', function() use ($router) {
-    $router->render('public/about');
+// run : http://localhost/user/{type-your-id}
+$router->get('/user/{id}', function($id) use ($router) {
+    $router->render('public/user');
 });
 
+// Not Found error with default template
 $router->setNotFound(function() use ($router) {
     $router->render('error');
+});
+
+// Admin routes
+$router->group('/admin', function() use ($router) {
+    
+    // run : http://localhost/admin/dashboard
+    $router->get('/dashboard', function() use ($router) {
+        $router->render('admin/dashboard');
+    });
+
+    // run : http://localhost/admin/settings
+    $router->get('/settings', function() use ($router) {
+        $router->render('admin/settings');
+    });
+    
 });
 
